c253148f46a91e8e83529fb74c6b9f47236d1775
Jason Proctor <jproctor@mtholyoke.edu>
2011-10-13 09:52:54 -0400

    Monkeypatch to handle Atom enclosures

--- lib/audrey2.rb
+++ lib/audrey2.rb
@@ -5,6 +5,71 @@ require 'open-uri'
 require 'haml'
 require 'optparse'
 
+# Prep SimpleRSS to deal with Atom enclosures
+class SimpleRSS
+  @@item_tags << :'link+enclosure'
+end
+
+# Monkeypatch FeedNormalizer to deal with Atom enclosures
+module FeedNormalizer
+  class SimpleRssParser
+    undef self.package
+    def self.package(atomrss)
+      feed = Feed.new(self)
+
+      # root elements
+      feed_mapping = {
+        :generator => :generator,
+        :title => :title,
+        :last_updated => [:updated, :lastBuildDate, :pubDate, :dc_date],
+        :copyright => [:copyright, :rights],
+        :authors => [:author, :webMaster, :managingEditor, :contributor],
+        :urls => :link,
+        :description => [:description, :subtitle],
+        :ttl => :ttl
+      }
+
+      map_functions!(feed_mapping, atomrss, feed)
+
+      # custom channel elements
+      feed.id = feed_id(atomrss)
+      feed.image = image(atomrss)
+
+
+      # entry elements
+      entry_mapping = {
+        :date_published => [:pubDate, :published, :dc_date, :issued],
+        :urls => :link,
+        :enclosures => [:enclosure, :'link+enclosure'],
+        :description => [:description, :summary],
+        :content => [:content, :content_encoded, :description],
+        :title => :title,
+        :authors => [:author, :contributor, :dc_creator],
+        :categories => :category,
+        :last_updated => [:updated, :dc_date, :pubDate]
+      }
+
+      atomrss.entries.each do |atomrss_entry|
+        unless atomrss_entry.title.nil? && atomrss_entry.description.nil? # some feeds return empty items
+          feed_entry = Entry.new
+          map_functions!(entry_mapping, atomrss_entry, feed_entry)
+  
+          # custom entry elements
+          feed_entry.id = atomrss_entry.guid || atomrss_entry[:id] # entries are a Hash..
+          # fall back to link for ID
+          feed_entry.id ||= atomrss_entry.link if atomrss_entry.respond_to?(:link) && atomrss_entry.link
+          feed_entry.copyright = atomrss_entry.copyright || (atomrss.respond_to?(:copyright) ? atomrss.copyright : nil)
+  
+          feed.entries << feed_entry
+        end
+      end
+
+      feed
+    end
+  end
+end
+
+
 module HashExtensions # Adapted from http://gist.github.com/151324 by Avdi Grimm and Paul Berry
   def recursively_symbolize_keys
     inject({}) do |acc, (k,v)|
